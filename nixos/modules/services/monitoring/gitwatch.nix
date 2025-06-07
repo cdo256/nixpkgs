e54da1e7a10b22e274a5a947b85f6ba05116f73c
Christina O'Donnell <cdo@mutix.org>
2025-06-07 13:30:23 +0100

    Add additional flags to gitwatch

--- nixos/modules/services/monitoring/gitwatch.nix
+++ nixos/modules/services/monitoring/gitwatch.nix
@@ -19,8 +19,19 @@ let
     nameValuePair "gitwatch-${name}" (
       let
         getvar = flag: var: optionalString (cfg."${var}" != null) "${flag} ${cfg."${var}"}";
+        getbool = flag: var: optionalString cfg."${var}" flag;
         branch = getvar "-b" "branch";
         remote = getvar "-r" "remote";
+        debounce = getvar "-s" "debounceTime";
+        dateFmt = getvar "-d" "dateFormat";
+        gitdir = getvar "-g" "gitDir";
+        msg = getvar "-m" "commitMessage";
+        events = getvar "-e" "events";
+        logColor = optionalString (cfg.logLines != null) "-l ${toString cfg.logLines}";
+        logNoColor = optionalString (cfg.logLinesNoColor != null) "-L ${toString cfg.logLinesNoColor}";
+        rebase = getbool "-R" "rebase";
+        noMerge = getbool "-M" "noMerge";
+        exclude = getvar "-x" "excludePattern";
       in
       rec {
         inherit (cfg) enable;
@@ -37,7 +48,12 @@ let
           if [ -n "${cfg.remote}" ] && ! [ -d "${cfg.path}" ]; then
             git clone ${branch} "${cfg.remote}" "${cfg.path}"
           fi
-          gitwatch ${remote} ${branch} ${cfg.path}
+          gitwatch \
+            ${remote} ${branch} \
+            ${debounce} ${dateFmt} ${gitdir} ${msg} \
+            ${logColor} ${logNoColor} ${events} \
+            ${rebase} ${noMerge} ${exclude} \
+            ${cfg.path}
         '';
         serviceConfig.User = cfg.user;
       }
@@ -56,13 +72,12 @@ in
         user = "user";
         path = "/home/user/watched-project";
         remote = "git@github.com:me/my-project.git";
-      };
-      disabled-repo = {
-        enable = false;
-        user = "user";
-        path = "/home/user/disabled-project";
-        remote = "git@github.com:me/my-old-project.git";
-        branch = "autobranch";
+        debounceTime = 2;
+        dateFormat = "+%Y-%m-%d %H:%M:%S";
+        rebase = true;
+        commitMessage = "Auto commit at %d";
+        logLines = 10;
+        excludePattern = "*.swp";
       };
     };
     type =
@@ -86,6 +101,56 @@ in
           };
           branch = mkOption {
             description = "Optional branch in remote repository";
+            type = nullOr str;
+            default = null;
+          };
+          debounceTime = mkOption {
+            description = "Time to wait in seconds after a change before committing";
+            type = nullOr int;
+            default = 2;
+          };
+          dateFormat = mkOption {
+            description = "The commit timestamp format string passed to `date`";
+            type = nullOr str;
+            default = "+%Y-%m-%d %H:%M:%S";
+          };
+          rebase = mkOption {
+            description = "Pull and rebase before pushing";
+            type = bool;
+            default = false;
+          };
+          gitDir = mkOption {
+            description = "Custom path to .git directory (used as --git-dir)";
+            type = nullOr str;
+            default = null;
+          };
+          commitMessage = mkOption {
+            description = "Commit message format with %d for timestamp";
+            type = nullOr str;
+            default = null;
+          };
+          logLines = mkOption {
+            description = "Maximum number of lines to log (color) in message";
+            type = nullOr int;
+            default = null;
+          };
+          logLinesNoColor = mkOption {
+            description = "Maximum number of lines to log (no color) in message";
+            type = nullOr int;
+            default = null;
+          };
+          events = mkOption {
+            description = "Custom inotify events string (e.g. modify,delete)";
+            type = nullOr str;
+            default = null;
+          };
+          noMerge = mkOption {
+            description = "Prevent commits if merge is in progress";
+            type = bool;
+            default = false;
+          };
+          excludePattern = mkOption {
+            description = "Exclude pattern for inotifywait.";
             type = nullOr str;
             default = null;
           };

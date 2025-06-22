da094ad76fe23124d07fdf9940bbcd2cefd0c48f
Christina O'Donnell <cdo@mutix.org>
2025-06-22 18:31:06 +0100

    Update agda2-mode to 2.8.0-rc3.

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/agda2-mode/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/agda2-mode/package.nix
@@ -1,15 +1,31 @@
-{ melpaBuild, haskellPackages }:
-let
-  Agda = haskellPackages.Agda;
-in
-melpaBuild {
-  pname = "agda2-mode";
-  inherit (Agda) src version;
+{
+  haskellPackages,
+  fetchFromGitHub,
+  melpaBuild,
+}:
 
-  files = ''("src/data/emacs-mode/*.el")'';
+let
+  inherit (haskellPackages) Agda;
+in
+melpaBuild rec {
+  pname = "agda2-mode";
+  version = "2.8.0";
+  src = fetchFromGitHub {
+    owner = "agda";
+    repo = "agda";
+    rev = "fe0f6a324480840fdcb855d1e72471be5bd6e39e";
+    hash = "sha256-PxbCKqWjJpmVYDz6/f41CiLT4euOGPDLHhzHj2T9ODQ=";
+  };
+
+  preBuild = ''
+    mv agda2-mode-pkg.el agda2-mode-${version}-pkg.el
+  '';
+
+  sourceRoot = "${src.name}/src/data/emacs-mode";
 
   meta = {
     inherit (Agda.meta) homepage license;
     description = "Agda2-mode for Emacs extracted from Agda package";
+    maintainers = [ ];
   };
 }
